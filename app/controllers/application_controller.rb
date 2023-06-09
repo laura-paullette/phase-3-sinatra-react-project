75d1672b08c2c0801bcc65ee5e12165a058a677f
laura-paullette <laura.paullette@student.moringaschool.com>
2023-06-09 11:09:23 +0300

    applicant form update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,26 +15,27 @@ class ApplicationController < Sinatra::Base
       trait: params[:trait],
       image: params[:image]
     )
-  end
-  
-  
-  patch '/dogs/:id' do
-    dog = Dog.find(params[:id])
-    dog.update(
-      name: params[:names],
-      breed: params[:breeds],
-      age: params[:ages], 
-      trait: params[:traits],
-      image: params[:images]
-    )
     dog.to_json
   end
+  
+  
+  # patch '/dogs/:id' do
+  #   dog = Dog.find(params[:id])
+  #   dog.update(
+  #     name: params[:names],
+  #     breed: params[:breeds],
+  #     age: params[:ages], 
+  #     trait: params[:traits],
+  #     image: params[:images]
+  #   )
+  #   dog.to_json
+  # end
 
-  delete "/dogs/:id" do
-    dog = Dog.find(params[:id])
-    dog.destroy
-    dog.to_json
-  end
+  # delete "/dogs/:id" do
+  #   dog = Dog.find(params[:id])
+  #   dog.destroy
+  #   dog.to_json
+  # end
 
   get '/dogs' do
     dogs =  Dog.all.order(:name).limit(1)
@@ -42,15 +43,26 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/dogs/:id' do
-    # look up the game in the database using its ID
     dog = Dog.find(params[:id])
-    # send a JSON-formatted response of the game data
-    dog.to_json
+    dog.to_json(include: :applicants)
   end
 
   get '/applicants' do
     applicant = Applicant.all
-    applicant.to_json(include: :dogs)
+    applicant.to_json
   end
 
+
+  post '/applicants' do
+    applicant = Applicant.create(
+      name: params[:name],
+      job: params[:job],
+      city: params[:city], 
+      pets_owned: params[:pets_owned],
+      image: params[:image],
+      dog_id: params[:dog_id]
+    )
+    applicant.to_json
+
+  end
 end
